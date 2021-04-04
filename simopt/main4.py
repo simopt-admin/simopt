d3a76de9e8fe74342033784660a10483e8dabfd8
Yunsoo-Ha <60554589+Yunsoo-Ha@users.noreply.github.com>
2021-04-04 16:09:07 -0400

    Update main4.py

--- simopt/main4.py
+++ simopt/main4.py
@@ -10,17 +10,17 @@ from base import Solution
 #    "cov" : [[2276, 1508, 813], [1508, 2206, 1349], [813, 1349, 1865]]
 #        }
 
-#myproblem = facilitysizingTotalCost(fixed_factors)
 myproblem = facilitysizingTotalCost()
-#print(myproblem.oracle.factors)
-
 rng_list = [MRG32k3a() for _ in range(myproblem.oracle.n_rngs)]
 myproblem.oracle.attach_rngs(rng_list)
 
-decision_factors = [150, 350, 400]
+decision_factors = [150, 400, 400]
 
 mysoln = Solution(x=decision_factors, problem=myproblem)
 
 myproblem.simulate(mysoln, m=100)
+# stochastic constraints must be less then 0
+# denote the risk of failing to satisfy demand is p(x)  
+# stocahstic constraint: p(x) - epsilon < 0 
 print('The mean of the objectives are {}'.format(mysoln.objectives_mean))
 print('The mean of the stochastic constraints are {}'.format(mysoln.stoch_constraints_mean))
