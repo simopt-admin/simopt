a79d0f030de3eb7f0e1a7bd95a121b6fd482a7bc
formatt18 <mtf62@cornell.edu>
2021-05-05 15:02:06 -0400

    sphinx/rtd

--- simopt/docs/conf.py
+++ simopt/docs/conf.py
@@ -66,4 +66,4 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 #html_static_path = ['_static']
-html_static_path = ['_static']
+html_static_path = []
