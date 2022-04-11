fc66dea8e4dd34c4e7d7e5baef1634ac08a9e22b
simopt-admin <simopt.github@gmail.com>
2022-04-11 11:28:46 -0500

    pull Nelder-Mead
    pull Nelder-Mead

--- simopt/directory.py
+++ simopt/directory.py
@@ -13,6 +13,7 @@ model_directory : dictionary
 # import solvers
 from solvers.astrodf import ASTRODF
 from solvers.randomsearch import RandomSearch
+from solvers.neldmd import NelderMead
 # import models and problems
 from models.cntnv import CntNV, CntNVMaxProfit
 from models.mm1queue import MM1Queue, MM1MinMeanSojournTime
@@ -31,7 +32,8 @@ from models.paramesti import ParameterEstimation, ParamEstiMinLogLik
 # directory dictionaries
 solver_directory = {
     "ASTRODF": ASTRODF,
-    "RNDSRCH": RandomSearch
+    "RNDSRCH": RandomSearch,
+    "NELDMD": NelderMead
 }
 problem_directory = {
     "CNTNEWS-1": CntNVMaxProfit,
