087230bd7dc48a5706d0376067f1e054b63ffbb9
NataliaLondono <nlondono97@tamu.edu>
2022-05-08 21:25:49 -0500

    Uncomment a section

--- simopt/models/ProdSys.py
+++ simopt/models/ProdSys.py
@@ -486,280 +486,280 @@ Maximize the expected revenue.
 """
 
 
-# class ProdSys(Problem):
-#     """
-#     Base class to implement simulation-optimization problems.
+class ProdSys(Problem):
+    """
+    Base class to implement simulation-optimization problems.
 
-#     Attributes
-#     ----------
-#     name : string
-#         name of problem
-#     dim : int
-#         number of decision variables
-#     n_objectives : int
-#         number of objectives
-#     n_stochastic_constraints : int
-#         number of stochastic constraints
-#     minmax : tuple of int (+/- 1)
-#         indicator of maximization (+1) or minimization (-1) for each objective
-#     constraint_type : string
-#         description of constraints types:
-#             "unconstrained", "box", "deterministic", "stochastic"
-#     variable_type : string
-#         description of variable types:
-#             "discrete", "continuous", "mixed"
-#     lower_bounds : tuple
-#         lower bound for each decision variable
-#     upper_bounds : tuple
-#         upper bound for each decision variable
-#     gradient_available : bool
-#         indicates if gradient of objective function is available
-#     optimal_value : float
-#         optimal objective function value
-#     optimal_solution : tuple
-#         optimal solution
-#     model : Model object
-#         associated simulation model that generates replications
-#     model_default_factors : dict
-#         default values for overriding model-level default factors
-#     model_fixed_factors : dict
-#         combination of overriden model-level factors and defaults
-#     model_decision_factors : set of str
-#         set of keys for factors that are decision variables
-#     rng_list : list of rng.MRG32k3a objects
-#         list of RNGs used to generate a random initial solution
-#         or a random problem instance
-#     factors : dict
-#         changeable factors of the problem
-#             initial_solution : list
-#                 default initial solution from which solvers start
-#             budget : int > 0
-#                 max number of replications (fn evals) for a solver to take
-#     specifications : dict
-#         details of each factor (for GUI, data validation, and defaults)
+    Attributes
+    ----------
+    name : string
+        name of problem
+    dim : int
+        number of decision variables
+    n_objectives : int
+        number of objectives
+    n_stochastic_constraints : int
+        number of stochastic constraints
+    minmax : tuple of int (+/- 1)
+        indicator of maximization (+1) or minimization (-1) for each objective
+    constraint_type : string
+        description of constraints types:
+            "unconstrained", "box", "deterministic", "stochastic"
+    variable_type : string
+        description of variable types:
+            "discrete", "continuous", "mixed"
+    lower_bounds : tuple
+        lower bound for each decision variable
+    upper_bounds : tuple
+        upper bound for each decision variable
+    gradient_available : bool
+        indicates if gradient of objective function is available
+    optimal_value : float
+        optimal objective function value
+    optimal_solution : tuple
+        optimal solution
+    model : Model object
+        associated simulation model that generates replications
+    model_default_factors : dict
+        default values for overriding model-level default factors
+    model_fixed_factors : dict
+        combination of overriden model-level factors and defaults
+    model_decision_factors : set of str
+        set of keys for factors that are decision variables
+    rng_list : list of rng.MRG32k3a objects
+        list of RNGs used to generate a random initial solution
+        or a random problem instance
+    factors : dict
+        changeable factors of the problem
+            initial_solution : list
+                default initial solution from which solvers start
+            budget : int > 0
+                max number of replications (fn evals) for a solver to take
+    specifications : dict
+        details of each factor (for GUI, data validation, and defaults)
 
-#     Arguments
-#     ---------
-#     name : str
-#         user-specified name for problem
-#     fixed_factors : dict
-#         dictionary of user-specified problem factors
-#     model_fixed factors : dict
-#         subset of user-specified non-decision factors to pass through to the model
+    Arguments
+    ---------
+    name : str
+        user-specified name for problem
+    fixed_factors : dict
+        dictionary of user-specified problem factors
+    model_fixed factors : dict
+        subset of user-specified non-decision factors to pass through to the model
 
-#     See also
-#     --------
-#     base.Problem
-#     """
-#     def __init__(self, name="ProdSys", fixed_factors={}, model_fixed_factors={}):
-#         self.name = name
-#         self.n_objectives = 1
-#         self.n_stochastic_constraints = 1
-#         self.minmax = (-1,)
-#         self.constraint_type = "deterministic"
-#         self.variable_type = "discrete"
-#         self.gradient_available = False
-#         self.optimal_value = None
-#         self.optimal_solution = None
-#         self.model_default_factors = {}
-#         self.model_decision_factors = {"interm_product"}
-#         self.factors = fixed_factors
-#         self.specifications = {
-#             "initial_solution": {
-#                 "description": "Initial solution.",
-#                 "datatype": list,
-#                 "default": [200, 0, 0, 0, 0, 0]
-#             },
-#             "budget": {
-#                 "description": "Max # of replications for a solver to take.",
-#                 "datatype": int,
-#                 "default": 10000
-#             },
-#             "alpha": {
-#                 "description": "risk level parameter",
-#                 "datatype": float,
-#                 "default": 0.10
-#             },
-#             "min_sslevel": {
-#                 "description": "minimum tolerable service level",
-#                 "datatype": float,
-#                 "default": 0.5
-#             }
-#         }
-#         self.check_factor_list = {
-#             "initial_solution": self.check_initial_solution,
-#             "budget": self.check_budget,
-#             "alpha": self.check_alpha,
-#             "min_sslevel": self.check_min_sslevel
-#         }
-#         super().__init__(fixed_factors, model_fixed_factors)
-#         # Instantiate model with fixed factors and over-riden defaults.
-#         self.model = ProdSys(self.model_fixed_factors)
-#         self.dim = self.model.factors["num_products"]
+    See also
+    --------
+    base.Problem
+    """
+    def __init__(self, name="ProdSys", fixed_factors={}, model_fixed_factors={}):
+        self.name = name
+        self.n_objectives = 1
+        self.n_stochastic_constraints = 1
+        self.minmax = (-1,)
+        self.constraint_type = "deterministic"
+        self.variable_type = "discrete"
+        self.gradient_available = False
+        self.optimal_value = None
+        self.optimal_solution = None
+        self.model_default_factors = {}
+        self.model_decision_factors = {"interm_product"}
+        self.factors = fixed_factors
+        self.specifications = {
+            "initial_solution": {
+                "description": "Initial solution.",
+                "datatype": list,
+                "default": [200, 0, 0, 0, 0, 0]
+            },
+            "budget": {
+                "description": "Max # of replications for a solver to take.",
+                "datatype": int,
+                "default": 10000
+            },
+            "alpha": {
+                "description": "risk level parameter",
+                "datatype": float,
+                "default": 0.10
+            },
+            "min_sslevel": {
+                "description": "minimum tolerable service level",
+                "datatype": float,
+                "default": 0.5
+            }
+        }
+        self.check_factor_list = {
+            "initial_solution": self.check_initial_solution,
+            "budget": self.check_budget,
+            "alpha": self.check_alpha,
+            "min_sslevel": self.check_min_sslevel
+        }
+        super().__init__(fixed_factors, model_fixed_factors)
+        # Instantiate model with fixed factors and over-riden defaults.
+        self.model = ProdSys(self.model_fixed_factors)
+        self.dim = self.model.factors["num_products"]
 
-#     def check_initial_solution(self):
-#         return len(self.factors["initial_solution"]) == self.dim
+    def check_initial_solution(self):
+        return len(self.factors["initial_solution"]) == self.dim
 
-#     def check_budget(self):
-#         return self.factors["budget"] > 0
+    def check_budget(self):
+        return self.factors["budget"] > 0
 
-#     def check_alpha(self):
-#         return len(self.factors["alpha"]) > 0
+    def check_alpha(self):
+        return len(self.factors["alpha"]) > 0
 
-#     def check_min_sslevel(self):
-#         return self.factors["min_sslevel"] > 0 and self.factors["min_sslevel"] <= 1 
+    def check_min_sslevel(self):
+        return self.factors["min_sslevel"] > 0 and self.factors["min_sslevel"] <= 1 
 
-#     def check_simulatable_factors(self):
-#         if len(self.lower_bounds) != self.dim:
-#             return False
-#         elif len(self.upper_bounds) != self.dim:
-#             return False
-#         else:
-#             return True
+    def check_simulatable_factors(self):
+        if len(self.lower_bounds) != self.dim:
+            return False
+        elif len(self.upper_bounds) != self.dim:
+            return False
+        else:
+            return True
 
-#     def vector_to_factor_dict(self, vector):
-#         """
-#         Convert a vector of variables to a dictionary with factor keys
+    def vector_to_factor_dict(self, vector):
+        """
+        Convert a vector of variables to a dictionary with factor keys
 
-#         Arguments
-#         ---------
-#         vector : tuple
-#             vector of values associated with decision variables
+        Arguments
+        ---------
+        vector : tuple
+            vector of values associated with decision variables
 
-#         Returns
-#         -------
-#         factor_dict : dictionary
-#             dictionary with factor keys and associated values
-#         """
-#         factor_dict = {
-#             "interm_product": vector[:]
-#         }
-#         return factor_dict
+        Returns
+        -------
+        factor_dict : dictionary
+            dictionary with factor keys and associated values
+        """
+        factor_dict = {
+            "interm_product": vector[:]
+        }
+        return factor_dict
 
-#     def factor_dict_to_vector(self, factor_dict):
-#         """
-#         Convert a dictionary with factor keys to a vector
-#         of variables.
+    def factor_dict_to_vector(self, factor_dict):
+        """
+        Convert a dictionary with factor keys to a vector
+        of variables.
 
-#         Arguments
-#         ---------
-#         factor_dict : dictionary
-#             dictionary with factor keys and associated values
+        Arguments
+        ---------
+        factor_dict : dictionary
+            dictionary with factor keys and associated values
 
-#         Returns
-#         -------
-#         vector : tuple
-#             vector of values associated with decision variables
-#         """
-#         vector = tuple(factor_dict["interm_product"])
-#         return vector
+        Returns
+        -------
+        vector : tuple
+            vector of values associated with decision variables
+        """
+        vector = tuple(factor_dict["interm_product"])
+        return vector
 
-#     def response_dict_to_objectives(self, response_dict):
-#         """
-#         Convert a dictionary with response keys to a vector
-#         of objectives.
+    def response_dict_to_objectives(self, response_dict):
+        """
+        Convert a dictionary with response keys to a vector
+        of objectives.
 
-#         Arguments
-#         ---------
-#         response_dict : dictionary
-#             dictionary with response keys and associated values
+        Arguments
+        ---------
+        response_dict : dictionary
+            dictionary with response keys and associated values
 
-#         Returns
-#         -------
-#         objectives : tuple
-#             vector of objectives
-#         """
-#         objectives = (response_dict["avg_leadtime"],)
-#         return objectives
+        Returns
+        -------
+        objectives : tuple
+            vector of objectives
+        """
+        objectives = (response_dict["avg_leadtime"],)
+        return objectives
 
-#     def response_dict_to_stoch_constraints(self, response_dict):
-#         """
-#         Convert a dictionary with response keys to a vector
-#         of left-hand sides of stochastic constraints: E[Y] >= 0
+    def response_dict_to_stoch_constraints(self, response_dict):
+        """
+        Convert a dictionary with response keys to a vector
+        of left-hand sides of stochastic constraints: E[Y] >= 0
 
-#         Arguments
-#         ---------
-#         response_dict : dictionary
-#             dictionary with response keys and associated values
+        Arguments
+        ---------
+        response_dict : dictionary
+            dictionary with response keys and associated values
 
-#         Returns
-#         -------
-#         stoch_constraints : tuple
-#             vector of LHSs of stochastic constraint
-#         """
-#         stoch_constraints = None
-#         return stoch_constraints
+        Returns
+        -------
+        stoch_constraints : tuple
+            vector of LHSs of stochastic constraint
+        """
+        stoch_constraints = None
+        return stoch_constraints
 
-#     def deterministic_stochastic_constraints_and_gradients(self, x):
-#         """
-#         Compute deterministic components of stochastic constraints for a solution `x`.
+    def deterministic_stochastic_constraints_and_gradients(self, x):
+        """
+        Compute deterministic components of stochastic constraints for a solution `x`.
 
-#         Arguments
-#         ---------
-#         x : tuple
-#             vector of decision variables
+        Arguments
+        ---------
+        x : tuple
+            vector of decision variables
 
-#         Returns
-#         -------
-#         det_stoch_constraints : tuple
-#             vector of deterministic components of stochastic constraints
-#         det_stoch_constraints_gradients : tuple
-#             vector of gradients of deterministic components of stochastic constraints
-#         """
-#         det_stoch_constraints = None
-#         det_stoch_constraints_gradients = None
-#         return det_stoch_constraints, det_stoch_constraints_gradients
+        Returns
+        -------
+        det_stoch_constraints : tuple
+            vector of deterministic components of stochastic constraints
+        det_stoch_constraints_gradients : tuple
+            vector of gradients of deterministic components of stochastic constraints
+        """
+        det_stoch_constraints = None
+        det_stoch_constraints_gradients = None
+        return det_stoch_constraints, det_stoch_constraints_gradients
 
-#     def deterministic_objectives_and_gradients(self, x):
-#         """
-#         Compute deterministic components of objectives for a solution `x`.
+    def deterministic_objectives_and_gradients(self, x):
+        """
+        Compute deterministic components of objectives for a solution `x`.
 
-#         Arguments
-#         ---------
-#         x : tuple
-#             vector of decision variables
+        Arguments
+        ---------
+        x : tuple
+            vector of decision variables
 
-#         Returns
-#         -------
-#         det_objectives : tuple
-#             vector of deterministic components of objectives
-#         det_objectives_gradients : tuple
-#             vector of gradients of deterministic components of objectives
-#         """
-#         det_objectives = (0,)
-#         det_objectives_gradients = ((0,) * self.dim,)
-#         return det_objectives, det_objectives_gradients
+        Returns
+        -------
+        det_objectives : tuple
+            vector of deterministic components of objectives
+        det_objectives_gradients : tuple
+            vector of gradients of deterministic components of objectives
+        """
+        det_objectives = (0,)
+        det_objectives_gradients = ((0,) * self.dim,)
+        return det_objectives, det_objectives_gradients
 
-#     def check_deterministic_constraints(self, x):
-#         """
-#         Check if a solution `x` satisfies the problem's deterministic constraints.
+    def check_deterministic_constraints(self, x):
+        """
+        Check if a solution `x` satisfies the problem's deterministic constraints.
 
-#         Arguments
-#         ---------
-#         x : tuple
-#             vector of decision variables
+        Arguments
+        ---------
+        x : tuple
+            vector of decision variables
 
-#         Returns
-#         -------
-#         satisfies : bool
-#             indicates if solution `x` satisfies the deterministic constraints.
-#         """
-#         ProdSys.check_total_inventory(self)
-#         return True
+        Returns
+        -------
+        satisfies : bool
+            indicates if solution `x` satisfies the deterministic constraints.
+        """
+        ProdSys.check_total_inventory(self)
+        return True
 
-#     def get_random_solution(self, rand_sol_rng):
-#         """
-#         Generate a random solution for starting or restarting solvers.
+    def get_random_solution(self, rand_sol_rng):
+        """
+        Generate a random solution for starting or restarting solvers.
 
-#         Arguments
-#         ---------
-#         rand_sol_rng : rng.MRG32k3a object
-#             random-number generator used to sample a new random solution
+        Arguments
+        ---------
+        rand_sol_rng : rng.MRG32k3a object
+            random-number generator used to sample a new random solution
 
-#         Returns
-#         -------
-#         x : tuple
-#             vector of decision variables
-#         """
-#         x = tuple([rand_sol_rng.randint(0, self.model.factors["total_inventory"]) for _ in range(self.dim)])
-#         return x
+        Returns
+        -------
+        x : tuple
+            vector of decision variables
+        """
+        x = tuple([rand_sol_rng.randint(0, self.model.factors["total_inventory"]) for _ in range(self.dim)])
+        return x
