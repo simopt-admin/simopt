e387e8a914524dad80f131d241595622ed520043
sshashaa <sara.shashaani@gmail.com>
2022-05-10 11:13:01 -0400

    Update demo_sscont_experiment.py

--- simopt/demo/demo_sscont_experiment.py
+++ simopt/demo/demo_sscont_experiment.py
@@ -1,6 +1,5 @@
 """
-This script runs three versions of random search, plus ASTRO-DF, on 25
-versions of the (s, S) inventory problem.
+This script runs five solvers on 20 versions of the (s, S) inventory problem.
 Produces plots appearing in the INFORMS Journal on Computing submission.
 """
 
@@ -21,10 +20,10 @@ from wrapper_base import Experiment, plot_area_scatterplots, post_normalize, plo
 
 # Create 25 problem instances by varying two factors, five levels.
 demand_means = [25.0, 50.0, 100.0, 200.0, 400.0]
-lead_means = [1.0, 3.0, 6.0, 9.0, 12.0]
+lead_means = [1.0, 3.0, 6.0, 9.0]
 
 # Three versions of random search with varying sample sizes.
-rs_sample_sizes = [10, 50, 100]
+rs_sample_sizes = [10, 50]
 
 # RUNNING AND POST-PROCESSING EXPERIMENTS
 
@@ -64,6 +63,36 @@ for dm in demand_means:
         # Setup and run ASTRO-DF.
         solver_fixed_factors = {"delta_max": 200.0}
         new_experiment = Experiment(solver_name="ASTRODF",
+                                    problem_name="SSCONT-1",
+                                    problem_rename=problem_rename,
+                                    solver_fixed_factors=solver_fixed_factors,
+                                    problem_fixed_factors=problem_fixed_factors,
+                                    model_fixed_factors=model_fixed_factors
+                                    )
+        # Run experiment with M = 10.
+        new_experiment.run(n_macroreps=10)
+        # Post replicate experiment with N = 100.
+        new_experiment.post_replicate(n_postreps=100)
+        experiments_same_problem.append(new_experiment)
+        
+        # Setup and run Nelder-Mead.
+        solver_fixed_factors = {"delta_max": 200.0}
+        new_experiment = Experiment(solver_name="NELDMD",
+                                    problem_name="SSCONT-1",
+                                    problem_rename=problem_rename,
+                                    solver_fixed_factors=solver_fixed_factors,
+                                    problem_fixed_factors=problem_fixed_factors,
+                                    model_fixed_factors=model_fixed_factors
+                                    )
+        # Run experiment with M = 10.
+        new_experiment.run(n_macroreps=10)
+        # Post replicate experiment with N = 100.
+        new_experiment.post_replicate(n_postreps=100)
+        experiments_same_problem.append(new_experiment)
+        
+        # Setup and run STRONG.
+        solver_fixed_factors = {"delta_max": 200.0}
+        new_experiment = Experiment(solver_name="STRONG",
                                     problem_name="SSCONT-1",
                                     problem_rename=problem_rename,
                                     solver_fixed_factors=solver_fixed_factors,
@@ -117,113 +146,150 @@ for dm in demand_means:
         new_experiment.problem.name = fr"SSCONT-1 with $\mu_D={round(dm)}$ and $\mu_L={round(lm)}$"
         experiments_same_solver.append(new_experiment)
 experiments.append(experiments_same_solver)
-
+# Load ASTRO-DF results.
+solver_rename = "NELDMD"
+experiments_same_solver = []
+for dm in demand_means:
+    for lm in lead_means:
+        problem_rename = f"SSCONT-1_dm={dm}_lm={lm}"
+        file_name = f"{solver_rename}_on_{problem_rename}"
+        # Load experiment.
+        new_experiment = read_experiment_results(f"experiments/outputs/{file_name}.pickle")
+        # Rename problem and solver to produce nicer plot labels.
+        new_experiment.solver.name = "Nelder-Mead"
+        new_experiment.problem.name = fr"SSCONT-1 with $\mu_D={round(dm)}$ and $\mu_L={round(lm)}$"
+        experiments_same_solver.append(new_experiment)
+experiments.append(experiments_same_solver)
+# Load ASTRO-DF results.
+solver_rename = "STRONG"
+experiments_same_solver = []
+for dm in demand_means:
+    for lm in lead_means:
+        problem_rename = f"SSCONT-1_dm={dm}_lm={lm}"
+        file_name = f"{solver_rename}_on_{problem_rename}"
+        # Load experiment.
+        new_experiment = read_experiment_results(f"experiments/outputs/{file_name}.pickle")
+        # Rename problem and solver to produce nicer plot labels.
+        new_experiment.solver.name = "STRONG"
+        new_experiment.problem.name = fr"SSCONT-1 with $\mu_D={round(dm)}$ and $\mu_L={round(lm)}$"
+        experiments_same_solver.append(new_experiment)
+experiments.append(experiments_same_solver)
 # PLOTTING
 
 n_solvers = len(experiments)
 n_problems = len(experiments[0])
 
-# All progress curves for one experiment. Problem instance 0.
-plot_progress_curves([experiments[solver_idx][0] for solver_idx in range(n_solvers)], plot_type="all", all_in_one=True)
+plot_area_scatterplots(experiments, all_in_one=True, plot_CIs=True, print_max_hw=True)
+plot_solvability_profiles(experiments, plot_type="cdf_solvability", solve_tol=0.1, all_in_one=True, plot_CIs=True, print_max_hw=True)
+plot_solvability_profiles(experiments, plot_type="quantile_solvability", solve_tol=0.1, beta=0.5, all_in_one=True, plot_CIs=True, print_max_hw=True)
+                          
+for i in range(n_problems):
+    plot_progress_curves([experiments[solver_idx][i] for solver_idx in range(n_solvers)], plot_type="mean", all_in_one=True, plot_CIs=True, print_max_hw=True)
+    # plot_progress_curves([experiments[solver_idx][i] for solver_idx in range(n_solvers)], plot_type="quantile", beta=0.9, all_in_one=True, plot_CIs=True, print_max_hw=True)
+    # plot_solvability_cdfs([experiments[solver_idx][i] for solver_idx in range(n_solvers)], solve_tol=0.2,  all_in_one=True, plot_CIs=True, print_max_hw=True)
 
-# All progress curves for one experiment. Problem instance 22.
-plot_progress_curves([experiments[solver_idx][22] for solver_idx in range(n_solvers)], plot_type="all", all_in_one=True)
 
-# Mean progress curves from all solvers on one problem. Problem instance 0.
-plot_progress_curves(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
-                     plot_type="mean",
-                     all_in_one=True,
-                     plot_CIs=True,
-                     print_max_hw=False
-                     )
+# # All progress curves for one experiment. Problem instance 0.
+# plot_progress_curves([experiments[solver_idx][0] for solver_idx in range(n_solvers)], plot_type="all", all_in_one=True)
 
-# Mean progress curves from all solvers on one problem. Problem instance 22.
-plot_progress_curves(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
-                     plot_type="mean",
-                     all_in_one=True,
-                     plot_CIs=True,
-                     print_max_hw=False
-                     )
+# # All progress curves for one experiment. Problem instance 22.
+# plot_progress_curves([experiments[solver_idx][22] for solver_idx in range(n_solvers)], plot_type="all", all_in_one=True)
 
-# Plot 0.9-quantile progress curves from all solvers on one problem. Problem instance 0.
-plot_progress_curves(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
-                     plot_type="quantile",
-                     beta=0.9,
-                     all_in_one=True,
-                     plot_CIs=True,
-                     print_max_hw=False
-                     )
+# # Mean progress curves from all solvers on one problem. Problem instance 0.
+# plot_progress_curves(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
+#                      plot_type="mean",
+#                      all_in_one=True,
+#                      plot_CIs=True,
+#                      print_max_hw=False
+#                      )
 
-# Plot 0.9-quantile progress curves from all solvers on one problem. Problem instance 22.
-plot_progress_curves(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
-                     plot_type="quantile",
-                     beta=0.9,
-                     all_in_one=True,
-                     plot_CIs=True,
-                     print_max_hw=False
-                     )
+# # Mean progress curves from all solvers on one problem. Problem instance 22.
+# plot_progress_curves(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
+#                      plot_type="mean",
+#                      all_in_one=True,
+#                      plot_CIs=True,
+#                      print_max_hw=False
+#                      )
 
-# Plot cdf of 0.2-solve times for all solvers on one problem. Problem instance 0.
-plot_solvability_cdfs(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
-                      solve_tol=0.2,
-                      all_in_one=True,
-                      plot_CIs=True,
-                      print_max_hw=False
-                      )
+# # Plot 0.9-quantile progress curves from all solvers on one problem. Problem instance 0.
+# plot_progress_curves(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
+#                      plot_type="quantile",
+#                      beta=0.9,
+#                      all_in_one=True,
+#                      plot_CIs=True,
+#                      print_max_hw=False
+#                      )
 
-# Plot cdf of 0.2-solve times for all solvers on one problem. Problem instance 22.
-plot_solvability_cdfs(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
-                      solve_tol=0.2,
-                      all_in_one=True,
-                      plot_CIs=True,
-                      print_max_hw=False
-                      )
+# # Plot 0.9-quantile progress curves from all solvers on one problem. Problem instance 22.
+# plot_progress_curves(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
+#                      plot_type="quantile",
+#                      beta=0.9,
+#                      all_in_one=True,
+#                      plot_CIs=True,
+#                      print_max_hw=False
+#                      )
 
-# Plot area scatterplots of all solvers on all problems.
-plot_area_scatterplots(experiments=experiments,
-                       all_in_one=True,
-                       plot_CIs=False,
-                       print_max_hw=False
-                       )
+# # Plot cdf of 0.2-solve times for all solvers on one problem. Problem instance 0.
+# plot_solvability_cdfs(experiments=[experiments[solver_idx][0] for solver_idx in range(n_solvers)],
+#                       solve_tol=0.2,
+#                       all_in_one=True,
+#                       plot_CIs=True,
+#                       print_max_hw=False
+#                       )
 
-# Plot cdf 0.1-solvability profiles of all solvers on all problems.
-plot_solvability_profiles(experiments=experiments,
-                          plot_type="cdf_solvability",
-                          all_in_one=True,
-                          plot_CIs=True,
-                          print_max_hw=False,
-                          solve_tol=0.1
-                          )
+# # Plot cdf of 0.2-solve times for all solvers on one problem. Problem instance 22.
+# plot_solvability_cdfs(experiments=[experiments[solver_idx][22] for solver_idx in range(n_solvers)],
+#                       solve_tol=0.2,
+#                       all_in_one=True,
+#                       plot_CIs=True,
+#                       print_max_hw=False
+#                       )
 
-# Plot 0.5-quantile 0.1-solvability profiles of all solvers on all problems.
-plot_solvability_profiles(experiments=experiments,
-                          plot_type="quantile_solvability",
-                          all_in_one=True,
-                          plot_CIs=True,
-                          print_max_hw=False,
-                          solve_tol=0.1,
-                          beta=0.5
-                          )
+# # Plot area scatterplots of all solvers on all problems.
+# plot_area_scatterplots(experiments=experiments,
+#                        all_in_one=True,
+#                        plot_CIs=False,
+#                        print_max_hw=False
+#                        )
 
-# Plot difference of cdf 0.1-solvability profiles of all solvers on all problems.
-# Reference solver = ASTRO-DF.
-plot_solvability_profiles(experiments=experiments,
-                          plot_type="diff_cdf_solvability",
-                          all_in_one=True,
-                          plot_CIs=True,
-                          print_max_hw=False,
-                          solve_tol=0.1,
-                          ref_solver="ASTRO-DF"
-                          )
+# # Plot cdf 0.1-solvability profiles of all solvers on all problems.
+# plot_solvability_profiles(experiments=experiments,
+#                           plot_type="cdf_solvability",
+#                           all_in_one=True,
+#                           plot_CIs=True,
+#                           print_max_hw=False,
+#                           solve_tol=0.1
+#                           )
 
-# Plot difference of 0.5-quantile 0.1-solvability profiles of all solvers on all problems.
-# Reference solver = ASTRO-DF.
-plot_solvability_profiles(experiments=experiments,
-                          plot_type="diff_quantile_solvability",
-                          all_in_one=True,
-                          plot_CIs=True,
-                          print_max_hw=False,
-                          solve_tol=0.1,
-                          beta=0.5,
-                          ref_solver="ASTRO-DF"
-                          )
+# # Plot 0.5-quantile 0.1-solvability profiles of all solvers on all problems.
+# plot_solvability_profiles(experiments=experiments,
+#                           plot_type="quantile_solvability",
+#                           all_in_one=True,
+#                           plot_CIs=True,
+#                           print_max_hw=False,
+#                           solve_tol=0.1,
+#                           beta=0.5
+#                           )
+
+# # Plot difference of cdf 0.1-solvability profiles of all solvers on all problems.
+# # Reference solver = ASTRO-DF.
+# plot_solvability_profiles(experiments=experiments,
+#                           plot_type="diff_cdf_solvability",
+#                           all_in_one=True,
+#                           plot_CIs=True,
+#                           print_max_hw=False,
+#                           solve_tol=0.1,
+#                           ref_solver="ASTRO-DF"
+#                           )
+
+# # Plot difference of 0.5-quantile 0.1-solvability profiles of all solvers on all problems.
+# # Reference solver = ASTRO-DF.
+# plot_solvability_profiles(experiments=experiments,
+#                           plot_type="diff_quantile_solvability",
+#                           all_in_one=True,
+#                           plot_CIs=True,
+#                           print_max_hw=False,
+#                           solve_tol=0.1,
+#                           beta=0.5,
+#                           ref_solver="ASTRO-DF"
+#                           )
