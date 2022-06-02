05804c0728876bde5d6111283af1fc6af29c92b2
sshashaa <sara.shashaani@gmail.com>
2022-06-02 14:14:13 -0400

    Update wrapper_base.py

--- simopt/wrapper_base.py
+++ simopt/wrapper_base.py
@@ -1900,7 +1900,8 @@ def plot_terminal_progress(experiments, plot_type="violin", normalize=True, all_
             terminal_values = [td for exp_idx in range(n_experiments) for td in terminal_data[exp_idx]]
             terminal_data_dict = {"Solvers": solver_names, "Terminal": terminal_values}
             terminal_data_df = pd.DataFrame(terminal_data_dict)
-            sns.violinplot(x="Solvers", y="Terminal", data=terminal_data_df, inner="stick", scale="width")
+            # sns.violinplot(x="Solvers", y="Terminal", data=terminal_data_df, inner="stick", scale="width", showmeans=True, bw = 0.2,  cut=2)
+            sns.violinplot(x="Solvers", y="Terminal", data=terminal_data_df, inner="stick", scale="width", showmeans=True, cut=0.1)
             if normalize:
                 plt.ylabel("Terminal Progress")
             else:
