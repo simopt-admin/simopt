b318c98416270e94bbb265758e824fe0a6ab2ff0
sshashaa <sara.shashaani@gmail.com>
2022-06-02 14:16:40 -0400

    Update demo_ironore_experiment.py

--- simopt/demo/demo_ironore_experiment.py
+++ simopt/demo/demo_ironore_experiment.py
@@ -183,7 +183,7 @@ plot_solvability_profiles(experiments=experiments, plot_type="diff_quantile_solv
 plot_terminal_scatterplots(experiments, all_in_one=True)
 
 for i in range(n_problems):
-    plot_progress_curves([experiments[solver_idx][i] for solver_idx in range(n_solvers)], plot_type="mean", all_in_one=True, plot_CIs=True, print_max_hw=True, normalize=False)
+    plot_progress_curves([experiments[solver_idx][i] for solver_idx in range(n_solvers)], plot_type="mean", all_in_one=True, plot_CIs=True, print_max_hw=True, normalize=True)
     plot_terminal_progress([experiments[solver_idx][i] for solver_idx in range(n_solvers)], plot_type="violin", normalize=True, all_in_one=True)
 
 
