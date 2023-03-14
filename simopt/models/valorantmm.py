9cb10962ec5c77c59ee953d7e4f05d1b24a99d59
nolangberry <98135033+nolangberry@users.noreply.github.com>
2023-03-13 21:23:37 -0500

    updates

--- simopt/models/valorantmm.py
+++ simopt/models/valorantmm.py
@@ -5,11 +5,9 @@ Simulate matching of Valorant players on an online platform.
 A detailed description of the model/problem can be found
 `here <https://simopt.readthedocs.io/en/latest/chessmm.html>`_.
 """
+from itertools import combinations
 import numpy as np
-from scipy import special
-
-from ..base import Model, Problem
-
+from trueskill import Rating, quality_1vs1, rate_1vs1
 
 class ValorantMatchmaking(Model):
     """
@@ -45,33 +43,13 @@ class ValorantMatchmaking(Model):
         if fixed_factors is None:
             fixed_factors = {}
         self.name = "VALORANT"
-        self.n_rngs = 2
+        self.n_rngs = 4
         self.n_responses = 2
         self.specifications = {
-            "elo_max": {
-                "description": "maximum value of triangular distribution for Elo rating",
-                "datatype": float,
-                "default": 2500.0
-            },
-            "elo_min": {
-                "description": "minimum value of triangular distribution for Elo rating",
-                "datatype": float,
-                "default": 0.0
-            },
-            "elo_mode": {
-                "description": "the mode of the triangular distribution of elos according to the time of day",
-                "datatype": list,
-                "default": [1300, 1200, 1800, 2000, 1900, 2600, 1400, 1200]
-            },
             "poisson_rate": {
                 "description": "rate of Poisson process for player arrivals according to the time of day",
                 "datatype": list,
                 "default": [20, 15, 18, 20, 20, 23, 30, 25]
-            },
-            "num_players": {
-                "description": "number of players",
-                "datatype": int,
-                "default": 1000
             },
             "allowable_diff": {
                 "description": "maximum allowable difference between Elo ratings",
@@ -84,53 +62,85 @@ class ValorantMatchmaking(Model):
                 "default": 5
             },
             "kd_mean": {
-                "description": "The mean kill/death ratio for multivariate normal distribution of k/d and win rate of players",
+                "description": "The mean kill/death ratio for multivariate normal distribution of k/d, elo and win rate of players",
                 "datatype": float,
                 "default": 1.0
             },
-            "kd_cov": {
-                "description": "The co-variance of k/d for multivariate normal distribution of k/d and win rate of players",
+            "kd_var": {
+                "description": "The variance of k/d for multivariate normal distribution of k/d, elo, and win rate of players",
                 "datatype": float,
                 "default": 0.2
             },
             "wr_mean": {
-                "description": "The mean win percentage for multivariate normal distribution of k/d and win rate of players",
+                "description": "The mean win percentage for multivariate normal distribution of k/d, elo, and win rate of players",
                 "datatype": float,
                 "default": 50.0
             },
-            "wr_cov": {
-                "description": "The co-variance of win rate for multivariate normal distribution of k/d and win rate of players",
+            "wr_var": {
+                "description": "The variance of win rate for multivariate normal distribution of k/d, elo, and win rate of players",
                 "datatype": float,
                 "default": 5.0
+            },
+            "elo_mean": {
+                "description": "The mean of the elo of players in a multivariate normal distribution of k/d, elo, and win rate of players",
+                "datatype": float,
+                "default": 1300
+            },
+            "elo_var": {
+                "description": "The variance of the elo of players in a multivariate normal distribution of k/d, elo, and win rate of players",
+                "datatype": float,
+                "default": 300
+            },
+            "kdwr_cov": {
+                "description": "The co-variance of win rate and k/d for multivariate normal distribution of k/d, elo, and win rate of players",
+                "datatype": float,
+                "default": 5.0
+            },
+            "kdelo_cov": {
+                "description": "The co-variance of elo and k/d for multivariate normal distribution of k/d, elo, and win rate of players",
+                "datatype": float,
+                "default": 5.0
+            },
+            "elowr_cov": {
+                "description": "The co-variance of win rate and elo for multivariate normal distribution of k/d, elo, and win rate of players",
+                "datatype": float,
+                "default": 5.0
+            },
+            "team_dif": {
+                "description": "Allowable difference in average player rating between teams matched against each other",
+                "datatype": float,
+                "default": 300.0
+            },
+            "wait_thresh": {
+                "description": "The amount of time a player waits before they get prioritized for matchmaking, in seconds",
+                "datatype": float,
+                "default": 300.0,
+            },
+            "slack": {
+                "description": "If someone is waiting more than the wait threshold, the allowable difference increases by this amount",
+                "datatype": float,
+                "default": 300.0,
             }
         }
         self.check_factor_list = {
-            "elo_max": self.check_elo_max,
-            "elo_min": self.check_elo_min,
-            "elo_mode": self.check_elo_mode,
             "poisson_rate": self.check_poisson_rate,
             "team_num": self.check_team_num,
-            "num_players": self.check_num_players,
             "kd_mean": self.check_kd_mean,
-            "kd_cov": self.check_kd_cov,
+            "kd_var": self.check_kd_var,
             "wr_mean": self.check_wr_mean,
-            "wr_cov": self.check_wr_cov,
-            "allowable_diff": self.check_allowable_diff
+            "wr_var": self.check_wr_var,
+            "allowable_diff": self.check_allowable_diff,
+            "elo_mean": self.check_elo_mean,
+            "elo_var": self.check_elo_var,
+            "kdwr_cov": self.check_kdwr_cov,
+            "kdelo_cov": self.check_kdelo_cov,
+            "elowr_cov": self.check_elowr_cov,
+            "team_dif": self.check_team_dif,
+            "wait_thresh": self.check_wait_thresh,
+            "slack": self.check_slack
         }
         # Set factors of the simulation model.
         super().__init__(fixed_factors)
-
-    def check_elo_max(self):
-        return self.factors["elo_max"] > 0
-
-    def check_elo_min(self):
-        return self.factors["elo_mode"] >= 0
-
-    def check_elo_mode(self):
-        for i in range(len(self.factors["elo_mode"])):
-            if self.factors["elo_mode"][i] < 0:
-                return False
-        return True
 
     def check_poisson_rate(self):
         for i in range(len(self.factors["poisson_rate"])):
@@ -141,23 +151,41 @@ class ValorantMatchmaking(Model):
     def check_team_num(self):
         return self.factors["team_num"] > 0
 
-    def check_num_players(self):
-        return self.factors["num_players"] > 0
-
     def check_kd_mean(self):
-        return self.factors["num_players"] > 0
+        return self.factors["kd_mean"] > 0
 
     def check_kd_cov(self):
-        return self.factors["num_players"] > 0
+        return self.factors["kd_cov"] > 0
 
     def check_wr_mean(self):
-        return self.factors["num_players"] > 0
+        return self.factors["wr_mean"] > 0
 
     def check_wr_cov(self):
-        return self.factors["num_players"] > 0
+        return self.factors["wr_cov"] > 0
 
     def check_allowable_diff(self):
         return self.factors["allowable_diff"] > 0
+
+    def check_elo_mean(self):
+        return self.factors["elo_mean"] > 0
+
+    def check_elo_var(self):
+        return self.factors["allowable_diff"] > 0
+
+    def check_kdwr_cov(self):
+        return self.factors["kdwr_cov"] > 0
+
+    def check_kdelo_cov(self):
+        return self.factors["kdelo_cov"] > 0
+
+    def check_elowr_cov(self):
+        return self.factors["elowr_cov"] > 0
+
+    def check_wait_thresh(self):
+        return self.factors["wait_thresh"] > 0
+
+    def check_slack(self):
+        return self.factors["slack"] > 0
 
     def replicate(self, rng_list):
         """
@@ -180,18 +208,136 @@ class ValorantMatchmaking(Model):
         # Designate separate random number generators.
         elo_rng = rng_list[0]
         arrival_rng = rng_list[1]
+        uni_rng = rng_list[2]
+        multi_rng = rng_list[3]
         # Initialize statistics.
         # Incoming players are initialized with a wait time of 0.
         wait_times = np.zeros(self.factors["num_players"])
         waiting_players = []
         total_diff = 0
         elo_diffs = []
+        arr_times = []
+        time = 0
+        teams = []
         # Simulate arrival and matching and players.
+
+        """
+        NSPP: Generate arrival times at the maximum rate at which they arrive
+        Multiply by probability for each hour to see if you accept or reject        
+        """
+        # thinning the non-stationary poisson process
+        max_lambda = max(self.factors["poisson_rate"])
+        t_star = []  # gen arrivals
+        times = []  # accepted arrivals
+        new_t = []
+        u = []
+        for i in range(len(5000)):
+            new_t.append(arrival_rng.expovariate(1 / max_lambda))
+        t_star.append(new_t[0])
+        for i in range(1, len(new_t)):
+            t_star.append(new_t[i]+new_t[i+1])
+        t_star = [i for i in t_star if i <= 24]
+        for i in range(len(t_star)):
+            u.append(uni_rng.uniform(0, 1))
+
+        for i in range(len(t_star)):
+            ind = int(np.floor(t_star[i]))
+            if self.factors["poisson_rate"][ind] / max_lambda >= u[i]:
+                times.append(t_star[i])
+        # generated arrival times
+        # other d.v.s spread of people within team and time waited: along with max allowable difference between teams
+        # need to generate elos, kd stats, and win rate stats... then create a stat that takes these into account when making teams
+        # make a team of p players using 10 choose 5. Try all combinations and see if teams can be made
+        count = len(times)
+        mean = [self.factors["kd_mean"], self.factors["wr_mean"], self.factors["elo_mean"]]
+        cov = [[self.factors["kd_var"], self.factors["kdwr_cov"], self.factors["kdelo_cov"]], [self.factors["kdwr_cov"], self.factors["wr_var"], self.factors["elowr_cov"]],
+               [self.factors["kdwr_cov"], self.factors["kdwr_cov"], self.factors["elo_var"]]]
+        # compound elo, kd, and wr
+        # need a loop that makes teams within the elo range
+        time_list = []
+        matched = []
+        time_waited = []
+        temp_ind = 0
+        for player in range(len(times)): # iterating through each player as they come in to create teams
+            time = times[player] # update time to new player
+            kd, wr, elo = elo_rng.mvnormalvariate(mean, cov, factorized = False) # create elo, wr, and kd
+            while kd or wr or elo < 0:
+                kd, wr, elo = elo_rng.mvnormalvariate(mean, cov, False)
+            player_rating = kd + wr + elo # compound into one value
+            waiting_players.append(player_rating) # place player in queue
+            time_list.append(time) # place time of player in queue in separate list
+            prio = np.any(abs(time_list[-1] - wait_time) > self.factors["wait_thresh"] for wait_time in time_list) # has anyone been waiting longer than the threshold?
+            prio_queue = [] # priority player queue
+            for time in range(len(time_list)):
+                if abs(time_list[-1] - time) > self.factors["wait_thresh"]:
+                    prio_queue.append(time_list[i])
+            if not prio_queue:
+                team_comp = list(combinations(waiting_players, self.factors["team_num"]))  # makes combinations of teams of x players
+            else:
+            # have the quality change instead of elo difference
+            # class to represent playre
+            player = {
+                "rating": 0,
+                "time": 0,
+                "name": "ted"
+            }
+            if prio is True: # if someone has been waiting longer than the threshold, they get prioritized and the allowable dif increases
+                for i in range(len(team_comp)): # iterate through each team combination
+                    team_count = 0
+                    for j in range(len(self.factors["team_num"])): # iterate through each team member
+                        # check team comps to see if teams are within allowable difference + slack
+                        if abs(team_comp[i][j] - team_comp[i][self.factors["team_num"]]) > (self.factors["allowable_diff"] + self.factors["slack"]):
+                            break
+                        else:
+                            team_count += 1
+                    if team_count == self.factors["team_num"]:
+                        for j in range(self.factors["team_num"]):
+                            # temp_ind = waiting_players.index(team_comp[i][j])
+                            waiting_players.remove(team_comp[i][j]) # need to delete the players in team from waiting queue
+                            # del time_list[temp_ind]
+                    teams.append(team_comp[i]) # add team to teams rdy to be matched
+                    # only remove people once optimal pair is found
+                if len(teams) >= 2:
+                    team_avg = []
+                    teamdif = []
+                    records = []
+                    lowest_dif = 0
+                    # I wave been waiting longer and closest matchups between teams
+                    matchups = list(combinations(teams,ant to prioritize players who h 2))  # creating all possible teams of two
+                    for i in range(len(teams)):  # obtain averages of each team that has been made
+                        avg = sum(teams[i]) / len(teams[i])
+                        team_avg.append(avg)
+                    for i in range(len(matchups)):
+                        if self.factors["team_dif"] >= abs(matchups[i][0] - matchups[i][1]):
+                            teamdif.append(abs(matchups[i][0] - matchups[i][1]))
+                            records.append(i)
+                    lowest_dif = teamdif.index(min(teamdif))  # index of the smallest difference between two teams
+                    matched.insert(len(matched), matchups[records[lowest_dif]][0], matchups[records[lowest_dif]][1])
+
+            else:
+                for i in range(len(team_comp)): # iterate through each team combination
+                    team_count = 0
+                    for j in range(len(self.factors["team_num"])): # iterate through each team member
+                        # check team comps to see if teams are within allowable difference
+                        if abs(team_comp[i][j] - team_comp[i][self.factors["team_num"]]) > self.factors["allowable_diff"]:
+                            break
+                        else:
+                            team_count += 1
+                    if team_count == self.factors["team_num"]:
+                        for j in range(self.factors["team_num"]):
+                            waiting_players.remove(team_comp[i][j]) # need to delete the players in team from waiting queue
+                    teams.append(team_comp[i]) # add team to teams rdy to be matched
+        # if first player in queue has been waiting > x then prioritize them and increase threshold for teams
+
+
+                # increase theshold if waiting a while
+                # need separate list of when the waiting players entered the queue
+        # need to match teams
         for player in range(self.factors["num_players"]):
             # Generate interarrival time of the player.
             time = arrival_rng.poissonvariate(self.factors["poisson_rate"])
             # Generate rating of the player via acceptance/rejection (not truncation).
-            player_rating = elo_rng.normalvariate(self.factors["elo_mean"], self.factors["elo_sd"])
+            player_rating = elo_rng.triangular(self.factors["elo_min"], self.factors["elo_max"], self.factors["elo_mode"])
             while player_rating < 0 or player_rating > 2400:
                 player_rating = elo_rng.normalvariate(self.factors["elo_mean"], self.factors["elo_sd"])
             # Attempt to match the incoming player with waiting players in FIFO manner.
