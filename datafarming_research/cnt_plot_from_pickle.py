cc76ce426b6d620c1e738dd473199242e974b6b9
nicolefelice <ndfelice@ncsu.edu>
2024-03-22 13:53:01 -0400

    gamma_1 vs default plots
    created solvability profiles for ASTRODF where gamma_1 = 4 and default ASTRODF over design of problem factors for cnt

---
+++ datafarming_research/cnt_plot_from_pickle.py
@@ -0,0 +1,38 @@
+"""
+This script is intended to load a design over problem factors to be run on
+the updated version of CNTNEWS-1.
+"""
+import os
+import sys
+import os.path as o
+sys.path.append(o.abspath(o.join(o.dirname(sys.modules[__name__].__file__), "..")))
+import pandas as pd
+from simopt.experiment_base import ProblemsSolvers, plot_solvability_profiles
+import pickle
+
+pickle_1 = './datafarming_research/gamma_1_4/group_gamma_1_4.pickle'
+with open(pickle_1, 'rb') as f:
+    experiment_1 = pickle.load(f)
+
+pickle_2 = './datafarming_research/ASTRODF_cnt_design_1/group_ASTRODF_on_CNTNEWS-1.pickle'
+with open(pickle_2, 'rb') as f:
+    experiment_2 = pickle.load(f)
+    
+# give new names to differentiate between solver variants
+for solver in experiment_1.experiments:
+    for dp in solver:
+        print(dp)
+        dp.solver.name = 'ASTRODF_gamma_1-4' 
+
+experiments = experiment_1.experiments + experiment_2.experiments
+
+print("Plotting results.")
+# Produce basic plots of the solvers on the problems.
+plot_solvability_profiles(experiments=experiments, plot_type="cdf_solvability")
+# Plots will be saved in the folder experiments/plots.
+print("Finished. Plots can be found in experiments/plots folder.")
+
+
+
+
+
