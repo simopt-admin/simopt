b2e7be2f5fe4eb12263b2b2def27bc375fa07dba
natthab <71283609+natthab@users.noreply.github.com>
2024-03-26 19:36:42 -0400

    Update Boom_FrankWolfe2.py

--- simopt/solvers/Boom_FrankWolfe2.py
+++ simopt/solvers/Boom_FrankWolfe2.py
@@ -53,7 +53,7 @@ class BoomFrankWolfe2(Solver):
             "max_iters": {
                 "description": "maximum iterations",
                 "datatype": int,
-                "default": 300
+                "default": 1000
             },  
             "LSmethod":{
                 "description": "methods for line search algorithm",
