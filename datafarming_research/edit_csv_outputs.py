59b7826d3ffeb30885c221e86dbbd3bd0c0d52bb
nicolefelice <ndfelice@ncsu.edu>
2024-03-28 22:53:38 -0400

    first cross design over solver and problem

--- datafarming_research/edit_csv_outputs.py
+++ datafarming_research/edit_csv_outputs.py
@@ -7,36 +7,66 @@ Created on Wed Mar 27 16:00:53 2024
 
 import pandas as pd
 
-results_filename = './gamma_1_4/gamma_1_4_results.csv' # location of csv file that you want to edit
+results_filename = './first_ASTRODF_and_cnt_design/ASTRODF_on_CNTNEWS-1_results.csv' # location of csv file that you want to edit
 problem_design_filename =  'expt1_test_inputs.txt' # location of file with  problem design points
-new_save_name = 'gamma_1_4_edited_results.csv' # save name of new edited file
-design = pd.read_csv(problem_design_filename, sep = ' ')
+solver_design_filename = 'ASTRODF_factor_settings_design.txt' #location of file with solver design points
+new_save_name = 'first_ASTRODF_cnt_design_results_edited.csv' # save name of new edited file
+problem_design = pd.read_csv(problem_design_filename, sep = ' ')
+solver_design = pd.read_csv(solver_design_filename, sep = '\t', header=None)
 results = pd.read_csv(results_filename)
-n_dp = len(design)
+prob_n_dp = len(problem_design)
+solver_n_dp = len(solver_design)
+print(solver_n_dp)
 
 # create new columns in results
 results['problem_dp'] = 0
-
-# seperate design by factor
-c_m = design.iloc[:, :4]
-c_r = design.iloc[:,4:8]
-p_v = design.iloc[:,8:12]
+results['solver_dp'] = 0
 
 
-for dp in range(n_dp):
+# seperate problem_design by factor
+c_m = problem_design.iloc[:, :4]
+c_r = problem_design.iloc[:,4:8]
+p_v = problem_design.iloc[:,8:12]
+
+# add col for problem dp
+for dp in range(prob_n_dp):
+    # problem design factors
     m_cost = str([c_m.iloc[dp,0], c_m.iloc[dp,1], c_m.iloc[dp,2], c_m.iloc[dp,3]])
     r_cost = str([c_r.iloc[dp,0], c_r.iloc[dp,1], c_r.iloc[dp,2], c_r.iloc[dp,3]])
     s_price = str([p_v.iloc[dp,0], p_v.iloc[dp,1], p_v.iloc[dp,2], p_v.iloc[dp,3]])
+
     
     for index, row in results.iterrows():
         current_c_m = str(row['material_cost'])
         current_c_r = str(row['recourse_cost'])
         current_p_v = str(row['salvage_price'])
 
+
         
         if m_cost == current_c_m and r_cost == current_c_r and s_price == current_p_v:
 
             results.at[index, 'problem_dp'] = dp 
+
+
+
+# add col for solver dp
+for dp in range(solver_n_dp):
+    # solver desing factors
+    gamma_1 = str(solver_design.iloc[dp,0])
+    gamma_2 = str(solver_design.iloc[dp,1])
+    eta_1 = str(solver_design.iloc[dp,2])
+    eta_2 = str(solver_design.iloc[dp,3])
+    for index, row in results.iterrows():
+        current_g_1 = str(row['gamma_1'])
+        current_g_2 = str(row['gamma_2'])
+        current_e_1 = str(row['eta_1'])
+        current_e_2 = str(row['eta_2'])
+        
+        if gamma_1 == current_g_1 and gamma_2 == current_g_2 and eta_1 == current_e_1 and eta_2 == current_e_2:
+
+            results.at[index, 'solver_dp'] = dp
+    
+    
 
 results.to_csv(new_save_name, index = False)
             
--- datafarming_research/regression.py
+++ datafarming_research/regression.py
@@ -13,8 +13,11 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor as VI
 from statsmodels.stats.anova import anova_lm
 
 default_results_filename = './default ASTRODF/default_ASTRODF_on_cnt_design.csv' # file location of csv results for problem design run on default version of ASTRODF
-results_filename = '' # file location of csv results for cross design of solver design & problem design
+results_filename = './first_ASTRODF_and_cnt_design/first_ASTRODF_cnt_design_results_edited.csv' # file location of csv results for cross design of solver design & problem design
+problem_ratio_design_filename = 'problem_ratio_design.csv'
 default_data = pd.read_csv(default_results_filename)
+exp_data = pd.read_csv(results_filename)
+ratio_design = pd.read_csv(problem_ratio_design_filename)
 
 # problem pedictors
 c_1 = 1 # cost of material one
@@ -26,7 +29,7 @@ s_r = 1 # salvage price ratio
 
 
 # find average default performance for corresponding problem (final relative optimality gap)
-n_dp = default_data['DesignPt#'].max()
+n_dp = default_data['DesignPt#'].max() + 1
 data_summary = pd.DataFrame()
 data_summary['Problem Design Point'] = range(n_dp)
 data_summary['Default Average Optimality Gap'] = 0
@@ -34,6 +37,63 @@ for dp in range(n_dp):
     opt_gaps = default_data.loc[default_data['DesignPt#'] == dp, 'Final Relative Optimality Gap']
     avg_opt_gap = opt_gaps.mean()
     data_summary.at[dp, 'Default Average Optimality Gap'] = avg_opt_gap
+    
+    
+    
+#  find solver for each problem dp with best average performance    
+sol_n_dp = exp_data['solver_dp'].max() + 1
+prob_n_dp = exp_data['problem_dp'].max() + 1
+data_summary['Average Best Optimality Gap'] = 0
+data_summary['Best Solver'] = 0
+
+for prob_dp in range(prob_n_dp): # cycle through problem versions
+    prob_df = exp_data.loc[exp_data['problem_dp'] == prob_dp, ['problem_dp', 'solver_dp', 'Final Relative Optimality Gap']]
+    
+    # get average performance for each solver for problem version
+    avg_gaps = pd.DataFrame()
+    avg_gaps['solver_dp'] = 0
+    avg_gaps['Average Optimality Gap'] = 0
+    for sol_dp in range(sol_n_dp):
+        sol_df = prob_df.loc[prob_df['solver_dp']== sol_dp, ['solver_dp','Final Relative Optimality Gap']]
+        avg_gap = sol_df['Final Relative Optimality Gap'].mean()
+        avg_gaps.at[sol_dp, 'solver_dp'] = sol_dp
+        avg_gaps.at[sol_dp, 'Average Optimality Gap'] = avg_gap
+    
+    # find best average on problem
+    best_gap = avg_gaps['Average Optimality Gap'].min()
+    best_solver = avg_gaps.loc[avg_gaps['Average Optimality Gap'] == best_gap, 'solver_dp'].iloc[0]
+    data_summary.at[prob_dp, 'Average Best Optimality Gap'] = best_gap
+    data_summary.at[prob_dp, 'Best Solver'] = best_solver
+        
+
+    
+    print(prob_df)
+
+# add problem ratios (x values) to data summary
+data_summary['c_m1'] = 0
+data_summary['Mratio'] = 0
+data_summary['Rratio'] = 0
+data_summary['Sratio'] = 0
+
+for index in range(prob_n_dp):
+    c_m1 = ratio_design.at[index, 'c_m1']
+    Mratio = ratio_design.at[index, 'Mratio']
+    Rratio = ratio_design.at[index, 'Rratio']
+    Sratio = ratio_design.at[index, 'Sratio']
+    print(c_m1)
+    data_summary.at[index, 'c_m1'] = c_m1
+    data_summary.at[index, 'Mratio'] = Mratio
+    data_summary.at[index, 'Rratio'] = Rratio
+    data_summary.at[index, 'Sratio'] = Sratio
+    
+# find best - default for each problem
+data_summary['Difference btwn Best and Default Avg Optimality Gap'] = 0
+for index, row in data_summary.iterrows():
+    diff = row['Average Best Optimality Gap'] - row['Default Average Optimality Gap']
+    data_summary.at[index, 'Difference btwn Best and Default Avg Optimality Gap'] = diff
+    
+
 print(data_summary)
 
+data_summary.to_csv('test_data_summary.csv', index = False)
 
