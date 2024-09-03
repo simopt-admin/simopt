ca96b812a362930617167f5dba9005883fc95e35
William Grochocinski <williamgrochocinski@gmail.com>
2024-09-03 14:08:44 -0400

    Added datafarming gem version checking

--- simopt/experiment_base.py
+++ simopt/experiment_base.py
@@ -6536,18 +6536,35 @@ def create_design(
         # Isn't on path, but could still be installed
         # Query the list of gems to see if the gem is installed
         results = subprocess.run(
-            'gem list -i "^datafarming$"',
+            'gem list',
             shell=True,
             capture_output=True,
         )
-        process_output = results.stdout.decode("utf-8").strip()
-        # Check if the output is true or false
-        if process_output == "false":
-            error_msg = f"Design type '{design_type}' is either not valid, not installed, or not in your system path."
-            # If the design is supposed to be in the datafarming gem, provide instructions on how to install it
-            if design_type in ["nolhs"]:
-                error_msg += " If you have not installed the 'datafarming' gem, you can install it by running:"
-                error_msg += "\n`gem install datafarming -v 1.4`"
+        # Break the gems into an array
+        gem_list = results.stdout.decode("utf-8").strip().splitlines()
+        # Get only the line with the datafarming
+        datafarming_line = [gem for gem in gem_list if gem.startswith("datafarming ")]
+        # If the new array is empty, then datafarming isn't installed
+        if (len(datafarming_line) == 0):
+            error_msg = "Datafarming gem is not installed. Please install it by running:"
+            error_msg += "\n`gem install datafarming -v 1.4`"
+            raise Exception(error_msg)
+        # The datafarming gem exists, but is there a valid version installed?
+        # Strip away all the information except for version(s)
+        datafarming_versions = datafarming_line[0].replace("datafarming (", "").replace("default: ", "").replace(")", "").split(", ")
+        # Check for valid versions (min <= version < max)
+        min_version = "1.0.0"
+        max_version = "2.0.0"
+        version_check_results = [min_version <= version < max_version for version in datafarming_versions]
+        if not any(version_check_results):
+            # Write the correct error message depending on plurality
+            if len(version_check_results) == 1:
+                error_msg = f"Datafarming gem is installed, but the installed version {datafarming_versions} is not supported."
+            else:
+                error_msg = f"Datafarming gem is installed, but the installed versions {datafarming_versions.sort()} are not supported."
+            error_msg += f" Please install version {min_version} <= x < {max_version}."
+            error_msg += " This can be done by running:"
+            error_msg += "\n`gem install datafarming -v 1.4`"
             raise Exception(error_msg)
 
     # Make directory to store the current design file.
