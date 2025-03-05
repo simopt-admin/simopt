e52259e6e21ba1262cf6504168696227167e08b6
William Grochocinski <wagrocho@ncsu.edu>
2025-03-05 15:40:42 -0500

    fixed formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ latex_engine = "xelatex"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 main(["-o", os.path.abspath("."), os.path.abspath(".."), "-f"])
