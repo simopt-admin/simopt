698b9f0ebbdbdc23ac0b72b09ec5046bdf97c337
William Grochocinski <wagrocho@ncsu.edu>
2025-03-05 10:36:21 -0500

    Enhance class property support with type hints and PEP8 naming conventions

--- simopt/utils.py
+++ simopt/utils.py
@@ -1,10 +1,18 @@
+from typing import TypeVar, Callable, Any
+
+T = TypeVar("T", bound=type)
+
+
 class ClassPropertyDescriptor:
-    def __init__(self, fget):
+    def __init__(self, fget: Callable[[type[T]], Any]) -> None:
+        """A descriptor that allows class-level attribute access like a property."""
         self.fget = fget
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Any, owner: type[T]) -> Any:  # noqa: ANN401
+        """Retrieve the computed class property when accessed on the class."""
         return self.fget(owner)
 
 
-def classproperty(func):
+def classproperty(func: Callable[[type[T]], Any]) -> ClassPropertyDescriptor:
+    """Decorator to create a class property using a descriptor."""
     return ClassPropertyDescriptor(func)
