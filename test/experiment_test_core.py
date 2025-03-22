a8eec607001cdee88189e59840890016cca3d90d
William Grochocinski <wagrocho@ncsu.edu>
2025-03-21 22:19:13 -0400

    fixed type errors in exp test core

--- test/experiment_test_core.py
+++ test/experiment_test_core.py
@@ -17,6 +17,7 @@ import math
 import unittest
 from abc import abstractmethod
 from pathlib import Path
+from typing import Any
 
 import yaml
 
@@ -96,7 +97,7 @@ class ExperimentTestMixin:
     setUp method of the ExperimentTest class.
     """
 
-    def test_run(self) -> None:
+    def test_run(self: Any) -> None:
         # Check actual run results against expected
         self.myexperiment.run(n_macroreps=self.num_macroreps)
         self.assertEqual(
@@ -198,7 +199,7 @@ class ExperimentTestMixin:
                     + ".",
                 )
 
-    def test_post_replicate(self) -> None:
+    def test_post_replicate(self: Any) -> None:
         # Simulate results from the run method
         self.myexperiment = ProblemSolver(
             self.expected_solver_name, self.expected_problem_name
@@ -252,7 +253,7 @@ class ExperimentTestMixin:
                     + ".",
                 )
 
-    def test_post_normalize(self) -> None:
+    def test_post_normalize(self: Any) -> None:
         # Simulate results from the post_replicate method
         self.myexperiment = ProblemSolver(
             self.expected_solver_name, self.expected_problem_name
@@ -271,23 +272,30 @@ class ExperimentTestMixin:
             [self.myexperiment], n_postreps_init_opt=self.num_postreps
         )
 
+        objective_curves = []
+        progress_curves = []
+
         # Loop through each curve object and convert it into a tuple
         for i in range(len(self.myexperiment.objective_curves)):
-            self.myexperiment.objective_curves[i] = (
-                self.myexperiment.objective_curves[i].x_vals,
-                self.myexperiment.objective_curves[i].y_vals,
+            objective_curves.append(
+                [
+                    self.myexperiment.objective_curves[i].x_vals,
+                    self.myexperiment.objective_curves[i].y_vals,
+                ]
             )
         for i in range(len(self.myexperiment.progress_curves)):
-            self.myexperiment.progress_curves[i] = (
-                self.myexperiment.progress_curves[i].x_vals,
-                self.myexperiment.progress_curves[i].y_vals,
+            progress_curves.append(
+                [
+                    self.myexperiment.progress_curves[i].x_vals,
+                    self.myexperiment.progress_curves[i].y_vals,
+                ]
             )
 
         for mrep in range(self.num_macroreps):
             # Check to make sure the same number of objective curves are present
             # This should probably always be 2 (x and y)
             self.assertEqual(
-                len(self.myexperiment.objective_curves[mrep]),
+                len(objective_curves[mrep]),
                 len(self.expected_objective_curves[mrep]),
                 "Number of objective curves for problem "
                 + self.expected_problem_name
@@ -296,10 +304,10 @@ class ExperimentTestMixin:
                 + " does not match.",
             )
             # Make sure that curves are only checked if they exist
-            if len(self.myexperiment.objective_curves[mrep]) > 0:
+            if len(objective_curves[mrep]) > 0:
                 # Make sure the lengths of the X and Y values are the same
                 self.assertEqual(
-                    len(self.myexperiment.objective_curves[mrep][0]),
+                    len(objective_curves[mrep][0]),
                     len(self.expected_objective_curves[mrep][0]),
                     "Length of X values for problem "
                     + self.expected_problem_name
@@ -308,7 +316,7 @@ class ExperimentTestMixin:
                     + " do not match.",
                 )
                 self.assertEqual(
-                    len(self.myexperiment.objective_curves[mrep][1]),
+                    len(objective_curves[mrep][1]),
                     len(self.expected_objective_curves[mrep][1]),
                     "Length of Y values for problem "
                     + self.expected_problem_name
@@ -317,13 +325,9 @@ class ExperimentTestMixin:
                     + " do not match.",
                 )
                 # Check X (0) and Y (1) values
-                for x_index in range(
-                    len(self.myexperiment.objective_curves[mrep][0])
-                ):
+                for x_index in range(len(objective_curves[mrep][0])):
                     # If the value is NaN, make sure we're expecting NaN
-                    if math.isnan(
-                        self.myexperiment.objective_curves[mrep][0][x_index]
-                    ):
+                    if math.isnan(objective_curves[mrep][0][x_index]):
                         self.assertTrue(
                             math.isnan(
                                 self.expected_objective_curves[mrep][0][x_index]
@@ -341,9 +345,7 @@ class ExperimentTestMixin:
                     # Otherwise, check the value normally
                     else:
                         self.assertAlmostEqual(
-                            self.myexperiment.objective_curves[mrep][0][
-                                x_index
-                            ],
+                            objective_curves[mrep][0][x_index],
                             self.expected_objective_curves[mrep][0][x_index],
                             5,
                             "X values for problem "
@@ -356,13 +358,9 @@ class ExperimentTestMixin:
                             + str(x_index)
                             + ".",
                         )
-                for y_index in range(
-                    len(self.myexperiment.objective_curves[mrep][1])
-                ):
+                for y_index in range(len(objective_curves[mrep][1])):
                     # If the value is NaN, make sure we're expecting NaN
-                    if math.isnan(
-                        self.myexperiment.objective_curves[mrep][1][y_index]
-                    ):
+                    if math.isnan(objective_curves[mrep][1][y_index]):
                         self.assertTrue(
                             math.isnan(
                                 self.expected_objective_curves[mrep][1][y_index]
@@ -380,9 +378,7 @@ class ExperimentTestMixin:
                     # Otherwise, check the value normally
                     else:
                         self.assertAlmostEqual(
-                            self.myexperiment.objective_curves[mrep][1][
-                                y_index
-                            ],
+                            objective_curves[mrep][1][y_index],
                             self.expected_objective_curves[mrep][1][y_index],
                             5,
                             "Y values for problem "
@@ -399,7 +395,7 @@ class ExperimentTestMixin:
             # Check to make sure the same number of progress curves are present
             # This should probably always be 2 (x and y)
             self.assertEqual(
-                len(self.myexperiment.progress_curves[mrep]),
+                len(progress_curves[mrep]),
                 len(self.expected_progress_curves[mrep]),
                 "Number of progress curves for problem "
                 + self.expected_problem_name
@@ -408,10 +404,10 @@ class ExperimentTestMixin:
                 + " does not match.",
             )
             # Make sure that curves are only checked if they exist
-            if len(self.myexperiment.progress_curves[mrep]) > 0:
+            if len(progress_curves[mrep]) > 0:
                 # Make sure the lengths of the X and Y values are the same
                 self.assertEqual(
-                    len(self.myexperiment.progress_curves[mrep][0]),
+                    len(progress_curves[mrep][0]),
                     len(self.expected_progress_curves[mrep][0]),
                     "Length of X values for problem "
                     + self.expected_problem_name
@@ -420,7 +416,7 @@ class ExperimentTestMixin:
                     + " do not match.",
                 )
                 self.assertEqual(
-                    len(self.myexperiment.progress_curves[mrep][1]),
+                    len(progress_curves[mrep][1]),
                     len(self.expected_progress_curves[mrep][1]),
                     "Length of Y values for problem "
                     + self.expected_problem_name
@@ -429,13 +425,9 @@ class ExperimentTestMixin:
                     + " do not match.",
                 )
                 # Check X (0) and Y (1) values
-                for x_index in range(
-                    len(self.myexperiment.progress_curves[mrep][0])
-                ):
+                for x_index in range(len(progress_curves[mrep][0])):
                     # If the value is NaN, make sure we're expecting NaN
-                    if math.isnan(
-                        self.myexperiment.progress_curves[mrep][0][x_index]
-                    ):
+                    if math.isnan(progress_curves[mrep][0][x_index]):
                         self.assertTrue(
                             math.isnan(
                                 self.expected_progress_curves[mrep][0][x_index]
@@ -453,7 +445,7 @@ class ExperimentTestMixin:
                     # Otherwise, check the value normally
                     else:
                         self.assertAlmostEqual(
-                            self.myexperiment.progress_curves[mrep][0][x_index],
+                            progress_curves[mrep][0][x_index],
                             self.expected_progress_curves[mrep][0][x_index],
                             5,
                             "X values for problem "
@@ -466,13 +458,9 @@ class ExperimentTestMixin:
                             + str(x_index)
                             + ".",
                         )
-                for y_index in range(
-                    len(self.myexperiment.progress_curves[mrep][1])
-                ):
+                for y_index in range(len(progress_curves[mrep][1])):
                     # If the value is NaN, make sure we're expecting NaN
-                    if math.isnan(
-                        self.myexperiment.progress_curves[mrep][1][y_index]
-                    ):
+                    if math.isnan(progress_curves[mrep][1][y_index]):
                         self.assertTrue(
                             math.isnan(
                                 self.expected_progress_curves[mrep][1][y_index]
@@ -490,7 +478,7 @@ class ExperimentTestMixin:
                     # Otherwise, check the value normally
                     else:
                         self.assertAlmostEqual(
-                            self.myexperiment.progress_curves[mrep][1][y_index],
+                            progress_curves[mrep][1][y_index],
                             self.expected_progress_curves[mrep][1][y_index],
                             5,
                             "Y values for problem "
