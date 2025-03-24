ff008a89725a8a9b1d55bf2b026f251c9852cd16
William Grochocinski <wagrocho@ncsu.edu>
2025-03-23 23:48:44 -0400

    fixed Probs/Solvs/Mods being specified instead of child types

--- simopt/directory.py
+++ simopt/directory.py
@@ -8,7 +8,9 @@ import simopt.solvers
 from simopt.base import Model, Problem, Solver
 
 
-def load_models_and_problems() -> tuple[dict[str, Model], dict[str, Problem]]:
+def load_models_and_problems() -> tuple[
+    dict[str, type[Model]], dict[str, type[Problem]]
+]:
     """Dynamically load models and problems from simopt.models."""
     models = {}
     problems = {}
@@ -28,7 +30,7 @@ def load_models_and_problems() -> tuple[dict[str, Model], dict[str, Problem]]:
     return models, problems
 
 
-def load_solvers() -> dict[str, Solver]:
+def load_solvers() -> dict[str, type[Solver]]:
     """Dynamically load solvers from simopt.solvers."""
     solvers = {}
 
