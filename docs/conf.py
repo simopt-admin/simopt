1089ed4534a86b1315db79fe57c4c5c53756ddce
William Grochocinski <wagrocho@ncsu.edu>
2025-04-09 21:10:20 -0400

    modified conf.py to work with new structure, seperated out API update into seperate script

--- docs/conf.py
+++ docs/conf.py
@@ -11,56 +11,38 @@
 # `make clean`
 # `make html`
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import sys
 from pathlib import Path
 
-from sphinx.ext.apidoc import main
+# -- Path setup --------------------------------------------------------------
 
-simopt_path = Path("..", "simopt").resolve()
-project_root = Path("..").resolve()
-
-# Insert paths if they are not already in sys.path
-for path in [simopt_path, project_root]:
-    path_str = str(path)
-    if path_str not in sys.path:
-        sys.path.insert(0, path_str)
+project_root = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(project_root))
 
 # -- Project information -----------------------------------------------------
 
 project = "SimOpt"
 copyright = "2025, simopt-admin"  # noqa: A001
 author = "simopt-admin"
-
-# The full version, including alpha/beta/rc tags
 release = "1.1.1"
-
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
+master_doc = "source/index"
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
 ]
 
-intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+autosummary_generate = True
+autodoc_typehints = "description"
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -74,37 +56,24 @@ napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
 napoleon_attr_annotations = True
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "*test*", "*main*"]
+# Be careful not to exclude important files
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "/test/*", "*main*"]
 
+# -- HTML output -------------------------------------------------------------
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "default"
-
-
-latex_engine = "xelatex"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-
 html_static_path = ["_static"]
 
+# LaTeX config (if using)
+latex_engine = "xelatex"
 
-output_dir = Path().resolve()
-source_dir = Path("..").resolve()
-
-main(["-o", str(output_dir), str(source_dir), "-f"])
+# -- DO NOT run sphinx-apidoc here ------------------------------------------
+# If you want to generate API docs, do it manually or in a separate script.
+# For example:
+# `sphinx-apidoc -o source ../simopt -f`
+#
+# Running it here can break builds on Read the Docs due to repeated calls.
