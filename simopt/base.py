start stoch constraint gradient fixes

simopt/base.py
@@ -1153,7 +1153,7 @@ class Problem(ABC):
                             solution.det_objectives_gradients,
                         )
                     ]
-                    # solution.objectives_gradients[solution.n_reps] = [[sum(pairs) for pairs in zip(stoch_obj, det_obj)] for stoch_obj, det_obj in zip(self.response_dict_to_objectives(vector_gradients), solution.det_objectives_gradients)]
+                    #solution.objectives_gradients[solution.n_reps] = [[sum(pairs) for pairs in zip(stoch_obj, det_obj)] for stoch_obj, det_obj in zip(self.response_dict_to_objectives(vector_gradients), solution.det_objectives_gradients)]
             if (
                 self.n_stochastic_constraints > 0
                 and solution.stoch_constraints is not None
@@ -1167,7 +1167,7 @@ class Problem(ABC):
                         solution.det_stoch_constraints,
                     )
                 ]
-                # solution.stoch_constraints_gradients[solution.n_reps] = [[sum(pairs) for pairs in zip(stoch_stoch_cons, det_stoch_cons)] for stoch_stoch_cons, det_stoch_cons in zip(self.response_dict_to_stoch_constraints(vector_gradients), solution.det_stoch_constraints_gradients)]
+                solution.stoch_constraints_gradients[solution.n_reps] = [[sum(pairs) for pairs in zip(stoch_stoch_cons, det_stoch_cons)] for stoch_stoch_cons, det_stoch_cons in zip(self.response_dict_to_stoch_constraints(vector_gradients), solution.det_stoch_constraints_gradients)]
             # Increment counter.
             solution.n_reps += 1
             # Advance rngs to start of next subsubstream.
@@ -1657,7 +1657,7 @@ class Solution:
         # self.stoch_constraints_var = np.full((problem.n_stochastic_constraints), np.nan)
         # self.stoch_constraints_stderr = np.full((problem.n_stochastic_constraints), np.nan)
         # self.stoch_constraints_cov = np.full((problem.n_stochastic_constraints, problem.n_stochastic_constraints), np.nan)
-        # self.stoch_constraints_gradients_mean = np.full((problem.n_stochastic_constraints, problem.dim), np.nan)
+        self.stoch_constraints_gradients_mean = np.full((problem.n_stochastic_constraints, problem.dim), np.nan)
         # self.stoch_constraints_gradients_var = np.full((problem.n_stochastic_constraints, problem.dim), np.nan)
         # self.stoch_constraints_gradients_stderr = np.full((problem.n_stochastic_constraints, problem.dim), np.nan)
         # self.stoch_constraints_gradients_cov = np.full((problem.n_stochastic_constraints, problem.dim, problem.dim), np.nan)
@@ -1777,7 +1777,7 @@ class Solution:
             self.stoch_constraints_cov = np.cov(
                 self.stoch_constraints[: self.n_reps], rowvar=False, ddof=1
             )
-            # self.stoch_constraints_gradients_mean = np.mean(self.stoch_constraints_gradients[:self.n_reps], axis=0)
+            self.stoch_constraints_gradients_mean = np.mean(self.stoch_constraints_gradients[:self.n_reps], axis=0)
             # self.stoch_constraints_gradients_var = np.var(self.stoch_constraints_gradients[:self.n_reps], axis=0, ddof=1)
             # self.stoch_constraints_gradients_stderr = np.std(self.stoch_constraints_gradients[:self.n_reps], axis=0, ddof=1) / np.sqrt(self.n_reps)
             # self.stoch_constraints_gradients_cov = np.array([np.cov(self.stoch_constraints_gradients[:self.n_reps, stcon], rowvar=False, ddof=1) for stcon in range(len(self.det_stoch_constraints))])

simopt/solvers/csa_lp.py
@@ -256,7 +256,6 @@ class CSA_LP(Solver):  # noqa: N801
         if n_violated_cons == 1:
             d = grads[0]
         else:
-            print("tried grad", grads)
             # normalization
             grads = grads / np.linalg.norm(grads, axis=1).reshape(
                 n_violated_cons, 1
@@ -325,11 +324,9 @@ class CSA_LP(Solver):  # noqa: N801
         Ce = problem.Ce
         de = problem.de
 
-        # lower = np.array(problem.lower_bounds)
-        # upper = np.array(problem.upper_bounds)
-        # temp adjustment for san problem
-        lower = np.array(13 * (100,))
-        upper = np.array(13 * (0.1,))
+        lower = np.array(problem.lower_bounds)
+        upper = np.array(problem.upper_bounds)
+
 
         recommended_solns = []
         intermediate_budgets = []
@@ -415,7 +412,7 @@ class CSA_LP(Solver):  # noqa: N801
                 infeasible_step = t
                 last_is_feasible = 0
             else:
-                t = self.factors["step_f"](k)
+                t = self.factors["step_f"](self,k=k)
 
             # step-size
             # t = self.factors["step_f"](k)
@@ -423,7 +420,6 @@ class CSA_LP(Solver):  # noqa: N801
             # print("grad: ", grad)
             # new_x = cur_x + t * direction
             new_x = self.prox_fn(t * grad, cur_x, Ci, di, Ce, de, lower, upper)
-
             candidate_solution = self.create_new_solution(tuple(new_x), problem)
             # Use r simulated observations to estimate the objective value.
             problem.simulate(candidate_solution, r)